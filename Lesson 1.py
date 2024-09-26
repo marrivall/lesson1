d96f10c8e3a3dcab357d5c30ffb20d060843bfcb
Myroslava <miroslavaet@gmail.com>
2024-09-26 21:48:45 +0300

    Added text

---
+++ Lesson 1.py
@@ -0,0 +1,18 @@
+print("Hello world")
+print()
+print("Hell'o' world")
+print()
+# \n - перенес. на новий рядок
+# \t - табуляція - 4 пробіли
+# \ - службовий символ зробити друкованим
+print("Hello\nworld")
+print()
+print("Hello\n\tworld")
+print()
+print("Hello\\n\\t\"world\"")
+
+print("Hello world", end="-")
+print ("test")
+print("Hello world","Tesxt1","Text2", sep=",")
+
+print ("Hello world")
