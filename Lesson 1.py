3c151128ae28654eef09c7f50664858b3ca28e7b
Myroslava <miroslavaet@gmail.com>
2024-09-28 13:34:24 +0300

    changes_

--- Lesson 1.py
+++ Lesson 1.py
@@ -1,18 +1,90 @@
-print("Hello world")
-print()
-print("Hell'o' world")
-print()
+#print("Hello world")
+#print()
+#print("Hell'o' world")
+#print()
 # \n - перенес. на новий рядок
 # \t - табуляція - 4 пробіли
 # \ - службовий символ зробити друкованим
-print("Hello\nworld")
-print()
-print("Hello\n\tworld")
-print()
-print("Hello\\n\\t\"world\"")
+#print("Hello\nworld")
+#print()
+#print("Hello\n\tworld")
+#print()
+#print("Hello\\n\\t\"world\"")
 
-print("Hello world", end="-")
-print ("test")
-print("Hello world","Tesxt1","Text2", sep=",")
+#print("Hello world", end="-")
+#print ("test")
+#print("Hello world","Tesxt1","Text2", sep=",")
 
-print ("Hello world")
+#print ("Hello world")
+
+
+
+#number = 10 + 2
+#print (number)
+#print(type(number))
+#number = 20.5
+#print (number)
+#print(type(number))
+#user_name = "marrivall" # NOT "number"
+#print (number)
+#print(type(number))
+#number = True
+#print (number)
+#print(type(number))
+
+#userName1 = "Monya"
+#print (userName1)
+#user_Name = "Kostya"
+#print (user_name)
+
+###
+#number1 = 10
+#print (number1)
+#print (number1 := 10) # моржовий оператор
+
+#274 = 2+7+4
+
+#number = 356
+#n1 = number // 100
+#n2 = number % 100 //10
+#n2_v2 = number // 10 % 10
+#n3 = number % 10
+
+#print (n1)
+#print (n2)
+#print (n3)
+
+#result = n1 + n2 + n3
+#print ("Result:", result )
+
+#name = input("Enter your name :=")
+#age = int(input ("Enter your age: "))
+#v1
+#print ("Hello,",name,"You are",age,"years old")
+#v2
+#print("Hello,"+ name + "You are" + str(age) + "years old!")
+
+
+#number1 = int(input("Enter first number:"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
