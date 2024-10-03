96b1391683ce17e7081e4a9bb0b0dae18e6c6e09
Myroslava <miroslavaet@gmail.com>
2024-10-03 21:41:58 +0300

    HM 02.10

---
+++ HM.py
@@ -0,0 +1,67 @@
+#HOMEWORK
+# Найпростіший калькулятор
+#V1
+# n1 = int(input("Enter your number"))
+# maths_operation = input("Select your choice (+, -, *, /" )
+# n2 = int(input("Enter your number"))
+# if maths_operation == "+":
+#     result = n1+n2
+# elif maths_operation == "-":
+#     result = n1 - n2
+# elif maths_operation == "*":
+#     result = n1 * n2
+# elif maths_operation == "/"!= 0:
+#     result = n1 / n2
+# else:
+#     print ("Invalid input, please, try again")
+#
+# print ("Result:", result)
+
+#V2
+# n1 = int(input("Enter your number"))
+# maths_operation = input("Select your choice (+, -, *, /" )
+# n2 = int(input("Enter your number"))
+# match maths_operation:
+#     case "+":
+#         result = n1+n2
+#     case "-":
+#         result = n1 - n2
+#     case "*":
+#         result = n1*n2
+#     case "/":
+#         result = n1/n2
+#     case "/!= 0":
+#           print ("Invalid input, please, try again")
+# print ("Result:", result )
+
+
+#Перемістити елемент у списку
+# numbers = [1,5,7,4,9,8]
+# numbers.insert (0, 8)
+# print (numbers)
+#
+# result = numbers.pop()
+# print (result)
+# print (numbers)
+
+
+
+#Розділити один список на два
+# 1)
+# nums = [2,5,8,3,9,4]
+# first_part = nums[:len(nums) // 2]
+# second_part = nums[:len(nums) // 2:]
+# result = [first_part,second_part]
+# print (result)
+#2)
+# nums = [3,7,6,4,9]
+# first_part = nums[:3]
+# second_part = nums [3:]
+# result = [first_part,second_part]
+# print (result)
+#3)
+# nums = []
+# first_part = nums [:len(nums)//2]
+# second_part = nums [len (nums) //2 :]
+# result = [first_part,second_part]
+# print (result)
