0eab452c6545966cf5e551d490e5f30441fc91d8
marrivall <miroslavaet@gmail.com>
2024-10-24 13:19:10 +0300

    Update HM.py

--- HM.py
+++ HM.py
@@ -1,3 +1,15 @@
+# Різниця між числами
+def difference (*nums):
+    if  len(nums) == 0:
+        return 0
+    difference = round(max(nums)-min(nums), 2)
+    return difference
+assert difference(1, 2, 3) == 2, 'Test1'
+assert difference(5, -5) == 10, 'Test2'
+assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
+assert difference() == 0, 'Test4'
+print('OK')
+
 # Визначити популярність певних слів у тексті
 def popular_words (text, words):
     text =  text.lower().split()
