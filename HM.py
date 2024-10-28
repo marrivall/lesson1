0ee69f0e14a87cdbcf9eaf605b88cef287899fb9
marrivall <miroslavaet@gmail.com>
2024-10-28 11:08:45 +0200

    Update HM.py

--- HM.py
+++ HM.py
@@ -1,3 +1,19 @@
+#Пошук першого слова
+def first_word(text):
+    text = text.replace('.', ' ').replace(',', ' ')
+    text = text.strip()
+    words = text.split()
+    first_word = words[0]
+    return first_word
+
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
+print('OK')
+
 #Генераторна функція
 def some_gen(begin, end, func):
     for x in range(end):
