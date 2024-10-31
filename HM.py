18ada14656c60d8b681c79286eb289c943437059
marrivall <miroslavaet@gmail.com>
2024-10-31 18:32:31 +0200

    Update HM.py

--- HM.py
+++ HM.py
@@ -1,3 +1,21 @@
+# Заповнення списку кубами чисел
+def generate_cube_numbers(end):
+    for x in range(2, end):
+        cube_number = x**3
+        if cube_number > end:
+            return
+        else:
+            yield cube_number
+
+from inspect import isgenerator
+
+gen = generate_cube_numbers(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(generate_cube_numbers(10)) == [8], 'оскільки воно менше 10.'
+assert list(generate_cube_numbers(100)) == [8, 27, 64], '5 у кубі це 125, а воно вже більше 100'
+assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000], '10 у кубі це 1000'
+print("OK")
+
 #Пошук першого слова
 def first_word(text):
     text = text.replace('.', ' ').replace(',', ' ')
