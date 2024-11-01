3a95a961cf249f2b17b3ae5b2b147d980b2da1f3
marrivall <miroslavaet@gmail.com>
2024-11-01 22:57:34 +0200

    Update HM.py

--- HM.py
+++ HM.py
@@ -1,3 +1,19 @@
+# Генератор простих чисел
+def prime_generator(end):
+    for num in range(2, end+1):
+        for n in range(2, num): #n- дільники
+            if num % n == 0:
+                break
+        else:
+            yield num
+from inspect import isgenerator
+gen = prime_generator(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
+assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
+assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
+print('Ok')
+
 # Перевірка на парність.
 def is_even(number):
     num_is_even = str(number)[-1] in "02468"
