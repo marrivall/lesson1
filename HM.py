b94bb52f89d4daa36f761db6314a70929b5ca817
marrivall <miroslavaet@gmail.com>
2024-11-07 20:42:30 +0200

    Update HM.py

--- HM.py
+++ HM.py
@@ -1,3 +1,57 @@
+# Створити клас цифрового лічильника.
+class Counter:
+   def __init__(self, current=1, min_value=0, max_value=10):
+       self.current = current
+       self.min_value = min_value
+       self.max_value = max_value
+
+   def set_current(self, start):
+       self.current = start
+
+   def set_max(self, max_max):
+       self.max_value = max_max
+
+   def set_min(self, min_min):
+       self.min_value = min_min
+
+   def step_up(self):
+       if self.current >= self.max_value:
+           raise ValueError ("Досягнуто максимуму")
+       else:
+           self.current = self.current + 1
+
+   def step_down(self):
+       if self.current <= self.min_value:
+           raise ValueError("Досягнуто мінімум")
+       else:
+           self.current = self.current - 1
+
+   def get_current(self):
+       return self.current
+
+counter = Counter()
+counter.set_current(7)
+counter.step_up()
+counter.step_up()
+counter.step_up()
+assert counter.get_current() == 10, 'Test1'
+try:
+    counter.step_up()  # ValueError
+except ValueError as e:
+    print(e) # Достигнут максимум
+assert counter.get_current() == 10, 'Test2'
+
+counter.set_min(7)
+counter.step_down()
+counter.step_down()
+counter.step_down()
+assert counter.get_current() == 7, 'Test3'
+try:
+    counter.step_down()  # ValueError
+except ValueError as e:
+    print(e) # Достигнут минимум
+assert counter.get_current() == 7, 'Test4'
+
 #Група студентів
 class Human:
     def __init__(self, gender, age, name, surname):
