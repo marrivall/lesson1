2094961cb8bf9a77bd0b0ed5b015c35bea864387
marrivall <miroslavaet@gmail.com>
2024-11-07 20:17:49 +0200

    Update HM.py

--- HM.py
+++ HM.py
@@ -1,3 +1,56 @@
+#Група студентів
+class Human:
+    def __init__(self, gender, age, name, surname):
+        self.gender = gender
+        self.age = age
+        self.name = name
+        self.surname = surname
+
+    def __str__(self):
+        return f'Human: Gender: {self.gender}, Age: {self.age}, Name: {self.name}, Surname: {self.surname}'
+
+class Student(Human):
+    def __init__(self, gender, age, name, surname, record_book):
+        super().__init__(gender, age, name, surname)
+        self.record_book = record_book
+
+    def __str__(self):
+        return f"Student: Name:{self.name}, Surname:{self.surname}, Record_book: {self.record_book}"
+
+class Group:
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        if isinstance(student, Student):
+            self.group.add(student)
+
+    def delete_student(self, surname):
+        self.group = {student for student in self.group if student.surname != surname}
+
+    def find_student(self, surname):
+        for student in self.group:
+            if student.surname == surname:
+                return student
+
+    def __str__(self):
+        all_students = '\n'.join(str(student) for student in self.group)
+        return f'Number:{self.number}\n {all_students} '
+
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+gr = Group('PD1')
+gr.add_student(st1)
+gr.add_student(st2)
+print(gr)
+assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
+assert gr.find_student('Jobs2') is None, 'Test2'
+assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод поиска должен возвращать экземпляр'
+gr.delete_student('Taylor')
+print(gr)
+gr.delete_student('Taylor')  # No error!
+
 #Очистити текст від html-тегів
 import codecs
 import re
