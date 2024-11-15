14b58be5faa73a2c13af206865ec7de6395a4593
marrivall <miroslavaet@gmail.com>
2024-11-15 18:47:31 +0200

    Update HM.py

--- HM.py
+++ HM.py
@@ -1,3 +1,57 @@
+# «Прямокутник
+nums = (int, float)
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_square(self):
+        return self.width * self.height
+
+    def __eq__(self, other):
+        if isinstance(other, Rectangle):
+            return self.get_square() == other.get_square()
+        else:
+            return False
+
+    def __add__(self, other):
+        if isinstance(other, Rectangle):
+            new_square = self.get_square() + other.get_square()
+            new_width = self.width
+            new_height = new_square//new_width
+            return Rectangle(new_width, new_height)
+        else:
+            raise ValueError("Illegal type of the argument")
+
+    def __mul__(self, n):
+        if isinstance(n, nums):
+            new_square = self.get_square() * n
+            new_width = self.width
+            new_height = new_square // new_width
+            return Rectangle(new_width, new_height)
+        else:
+            raise ValueError("Illegal type of the argument")
+
+    def __str__(self):
+        return f"Rectangle({self.width}, {self.height})"
+
+
+r1 = Rectangle(2, 4)
+r2 = Rectangle(3, 6)
+assert r1.get_square() == 8, 'Test1'
+assert r2.get_square() == 18, 'Test2'
+
+r3 = r1 + r2
+assert r3.get_square() == 26, 'Test3'
+
+r4 = r1 * 4
+assert r4.get_square() == 32, 'Test4'
+
+assert Rectangle(3, 6) == Rectangle(2, 9), 'Test5'
+print ('ok')
+
+
+
 # Клас «Правильний дріб»
 class Fraction:
     def __init__(self, numer , den): #numer - numerator, den - denominator
